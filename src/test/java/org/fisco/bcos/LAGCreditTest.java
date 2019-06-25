9d790e20c5aca5a7320aaa859febe0923ce6eb6f
Jia Yuran <s4j91@126.com>
2019-06-25 16:23:39 +0800

    update1

--- src/test/java/org/fisco/bcos/LAGCreditTest.java
+++ src/test/java/org/fisco/bcos/LAGCreditTest.java
@@ -1,28 +1,26 @@
 package org.fisco.bcos;
 
+import static org.junit.Assert.assertTrue;
+
 import org.fisco.bcos.temp.LAGCredit;
 import org.fisco.bcos.web3j.crypto.Credentials;
 import org.fisco.bcos.web3j.protocol.Web3j;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.Assert.assertTrue;
-
 public class LAGCreditTest extends BaseTest {
 
-  @Autowired private Web3j web3j;
-  @Autowired private Credentials credentials;
+    @Autowired private Web3j web3j;
+    @Autowired private Credentials credentials;
 
-  @Test
-  public void deployAndCallHelloWorld() throws Exception {
-    // deploy contract
-    LAGCreditController lagcreditcontrol = new LAGCreditController(web3j, credentials);
-    LAGCredit sad = lagcreditcontrol.deploy();
-
-    LAGCredit ewe = lagcreditcontrol.load(sad.getContractAddress());
-
-    long result = lagcreditcontrol.getTotalSupply(ewe.getContractAddress());
-    long origin = 100000;
-    assertTrue(origin == result);
-  }
+    @Test
+    public void deployAndCallHelloWorld() throws Exception {
+        // deploy contract
+        LAGCreditController lagcreditcontrol = new LAGCreditController(web3j, credentials);
+        LAGCredit sad = lagcreditcontrol.deploy();
+        LAGCredit ewe = lagcreditcontrol.load(sad.getContractAddress());
+        long result = lagcreditcontrol.getTotalSupply(ewe.getContractAddress());
+        long origin = 100000;
+        assertTrue(origin == result);
+    }
 }
